ca3f356ff638efdc6afa8d9cdb9e5bf39a03111f
Brett Bourne <brett@uvweb.org>
2013-03-06 16:29:39 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+First::Application.config.secret_token = 'd612caebe45ce20d3f4888b0a69d37fb03f2fb10141564d1009adb377e8cf0243f5b5c81ee77fc2a5b9beefcdcf90dbd156d88a49ef5d77886999d16fe64cda9'
